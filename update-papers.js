2b257ea3c8022487ef8f85f473d4c4ca41bb3fdf
Elvira Fleury <elvirafleury@dhcp-10-250-59-120.harvard.edu>
2025-02-13 10:41:02 -0500

    Update papers.html and update-papers.js to fetch and display arXiv papers over HTTPS

--- update-papers.js
+++ update-papers.js
@@ -7,9 +7,6 @@ async function fetchPapers() {
 
     try {
         const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
         const data = await response.text();
         console.log("Query successful. Data received:");
         console.log(data);
